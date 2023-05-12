7d0e751dc37143608a7fa34e6adadc6bba0a44b5
Agamjot158 <106953882+Agamjot158@users.noreply.github.com>
2023-05-12 11:43:16 +0530

    Update sensor.js

--- mqtt/models/sensor.js
+++ mqtt/models/sensor.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+module.exports = mongoose.model('sensor', new mongoose.Schema({
+  name: String,
+  floor: String,
+  room: String,
+  status: Boolean,
+  gas: Array,
+}, { collection : 'sens' }));
